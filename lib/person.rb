91ac353cd697f5ab09446789d98b4a00eb08d7b8
Rommel Haynes <rhaynes3k@gmail.com>
2020-01-01 21:51:26 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,37 +2,37 @@ class Person
   attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, 
 :wrist_size, :glove_size, :pant_length, :pant_width
 
-def intialize(attributes)
+#def intialize(attributes)
   
-end
+#end
 
-# def initialize(name:, 
-# weight:, 
-# wrist_size:, 
-# birthday:, 
-# handed:, 
-# glove_size:, 
-# hair_color:, 
-# complexion:, 
-# pant_length:, 
-# eye_color:, 
-# t_shirt_size:, 
-# pant_width:, 
-# height: 
-# )
-# @name=name
-# @weight=weight
-# @wrist_size=wrist_size
-# @birthday=birthday
-# @handed=handed
-# @glove_size=glove_size
-# @hair_color=hair_color
-# @complexion=complexion
-# @pant_length=pant_length
-# @eye_color=eye_color
-# @t_shirt_size=t_shirt_size
-# @pant_width=pant_width
-# @height=height
-# end
+def initialize(name:, 
+weight:, 
+wrist_size:, 
+birthday:, 
+handed:, 
+glove_size:, 
+hair_color:, 
+complexion:, 
+pant_length:, 
+eye_color:, 
+t_shirt_size:, 
+pant_width:, 
+height: 
+)
+@name=name
+@weight=weight
+@wrist_size=wrist_size
+@birthday=birthday
+@handed=handed
+@glove_size=glove_size
+@hair_color=hair_color
+@complexion=complexion
+@pant_length=pant_length
+@eye_color=eye_color
+@t_shirt_size=t_shirt_size
+@pant_width=pant_width
+@height=height
+end
 
 end
