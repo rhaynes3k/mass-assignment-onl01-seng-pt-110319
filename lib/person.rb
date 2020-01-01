8a3c1a463ddbb0380bd4c10a8f4eaef563bf8680
Rommel Haynes <rhaynes3k@gmail.com>
2020-01-01 21:13:25 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,5 +2,19 @@ class Person
   attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, 
 :wrist_size, :glove_size, :pant_length, :pant_width
 
-def initialize()
+def initialize(name:, 
+weight:, 
+wrist_size:, 
+birthday:, 
+handed:, 
+glove_size:, 
+hair_color:, 
+complexion:, 
+pant_length:, 
+eye_color:, 
+t_shirt_size:, 
+pant_width:, 
+height: 
+)
+
 end
