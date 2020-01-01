5dfd9a62e5d1dc38549facbcfa8bc09a25dff69a
Rommel Haynes <rhaynes3k@gmail.com>
2020-01-01 22:14:16 +0000

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,23 @@
+require 'pry'
 class Person
-  #your code here
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion,:t_shirt_size,:wrist_size, :glove_size, :pant_length, :pant_width
+
+def initialize(attributes)
+  attributes.each {|key, value| self.send(("#{key}="), value)}
+  
+@name=name
+@weight=weight
+@wrist_size=wrist_size
+@birthday=birthday
+@handed=handed
+@glove_size=glove_size
+@hair_color=hair_color
+@complexion=complexion
+@pant_length=pant_length
+@eye_color=eye_color
+@t_shirt_size=t_shirt_size
+@pant_width=pant_width
+@height=height
+end
+
 end
