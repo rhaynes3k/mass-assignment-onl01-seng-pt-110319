27d3528eee6a0df8b80a63729759f43be4dc9979
Rommel Haynes <rhaynes3k@gmail.com>
2020-01-01 22:08:17 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Person
   attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, 
 :wrist_size, :glove_size, :pant_length, :pant_width
@@ -21,6 +22,8 @@ def initialize(attributes)
 @t_shirt_size=t_shirt_size
 @pant_width=pant_width
 @height=height
+
+binding.pry
 end
 
 end
